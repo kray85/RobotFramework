affbeb33f61037203f28576d60f7080e785bef71
kray85 <kray85@gmail.com>
2025-04-18 14:16:00 -0400

    added custom toml config

--- customLibraries/LoginPage.py
+++ customLibraries/LoginPage.py
@@ -1,3 +1,4 @@
+import toml
 import SeleniumLibrary
 from robot.api.deco import library, keyword
 from robot.libraries.BuiltIn import BuiltIn
@@ -6,6 +7,7 @@ from robot.libraries.BuiltIn import BuiltIn
 class LoginPage():
     def __init__(self):
         self.selLib = BuiltIn().get_library_instance("SeleniumLibrary")
+        self.config_path = "config.toml"
 
     @keyword
     def get_input_values(self,radio_buttons_el):
@@ -15,3 +17,13 @@ class LoginPage():
         values = [element.get_attribute('value') for element in radio_buttons]
         return values
 
+    @keyword(name="Get User From Config")
+    def get_user_from_config(self, section, key):
+        # Load the TOML file
+        with open(self.config_path, "r") as file:
+            config = toml.load(file)
+
+        # Get the value from the specified section and key
+        value = config[section][key]
+        return value
+    
